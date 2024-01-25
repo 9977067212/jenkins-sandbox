545a3a8ecb13c829930fe30e1a586d78c9184147
9977067212 <146814651+9977067212@users.noreply.github.com>
2024-01-25 10:59:00 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,4 @@
 #! /bin/bash
 
-echo $SSH_PRIVATE_KEY
-echo $SSH_USER
-
-echo $SSH_USER > secret.txt
-
-cat secret.txt
-cat $SSH_PRIVATE_KEY
+echo "SSH private key: $SSH_PRIVATE_KEY"
+echo "SSH user: $SSH_USER"
