ac3b919861bd1c1ef51d039349167765936d8c60
9977067212 <146814651+9977067212@users.noreply.github.com>
2024-01-28 01:33:19 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,5 +4,5 @@ echo "Path to SSH private key: $SSH_PRIVATE_KEY"
 echo "SSH user: $SSH_USER"
 cat $SSH_PRIVATE_KEY
 
-export SECRET=$SSH_PRIVATE_KEY
-echo $SECRET
+echo "a ${SSH_PRIVATE_KEY} a" > file
+cat file
